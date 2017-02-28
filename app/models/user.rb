1cf19e2cf37d591e9345da3937ddc5fbfd0f6c35
nauticalnova <nauticalnova@hotmail.com>
2017-02-27 20:54:33 -0700

    started fresh

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-	has_many :songs
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,3 @@
 Rails.application.routes.draw do
-  get 'songs/index'
-
-  get 'songs/new'
-
-  get 'songs/create'
-
-  get 'songs/edit'
-
-  get 'songs/show'
-
-  get 'songs/delete'
-
-  get 'songs/update'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  devise_for :controllers
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
 end
